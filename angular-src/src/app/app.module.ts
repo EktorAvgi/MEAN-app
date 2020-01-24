3f0cebcfadc80aec235ec7a5177aeea3796e9635
Ektor <aektoras@gmail.com>
2020-01-24 20:39:43 +0000

    add the protection requests for loging in

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -15,13 +15,18 @@ import { ProfileComponent } from "./components/profile/profile.component";
 import { ValidateService } from "./services/validate.service";
 import { AuthService } from "./services/auth.service";
 import { FlashMessagesModule } from "angular2-flash-messages/module";
+import { AuthGuard } from "./guards/auth.guard";
 
 const appRoutes: Routes = [
   { path: "", component: HomeComponent },
   { path: "register", component: RegisterComponent },
   { path: "login", component: LoginComponent },
-  { path: "dashboard", component: DashboardComponent },
-  { path: "profile", component: ProfileComponent }
+  {
+    path: "dashboard",
+    component: DashboardComponent,
+    canActivate: [AuthGuard]
+  },
+  { path: "profile", component: ProfileComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
@@ -41,7 +46,7 @@ const appRoutes: Routes = [
     FlashMessagesModule.forRoot(),
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [ValidateService, AuthService],
+  providers: [ValidateService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
